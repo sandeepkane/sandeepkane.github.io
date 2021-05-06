b37c5e7cd078f95ca8d586de3b28e36c26a20e8b
SandeepKane-at-587358001311 <sandeep.kane@wipro.com>
2021-05-06 15:45:43 +0530

    Sidebar and footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
